adb455fc59557dbe9428ff4a0eb5de6c7801b8b2
unknown <jahandarmv@code.edu.az>
2023-01-04 16:12:17 +0400

    New Task

---
+++ HomeWork/Program.cs
@@ -0,0 +1,60 @@
+﻿using HomeWork.Controller;
+
+
+#region Calculate
+
+//CalculateController controller = new CalculateController();
+
+//Console.WriteLine(controller.Calculate());
+
+
+
+#endregion
+
+
+
+#region Fibonacci
+
+//FibonacciController fibonacci = new FibonacciController();
+//fibonacci.Fibonacci();
+
+
+
+#endregion
+
+
+
+#region Student Task
+
+StudentsController controller = new StudentsController();
+//controller.GetStudentByCounting();
+
+//controller.GetStudentsTotalAge();
+
+//controller.GetStudentsEmailName();
+
+//controller.GetStudentsAddress();    
+
+//controller.GetStudentsByNamesFiltered();
+
+
+//controller.GetStudentNamesSource();
+
+
+#endregion
+
+
+
+#region Employee
+
+//EmployeeController employee = new EmployeeController();
+//employee.GetEmployees();
+
+#endregion
+
+
+
+
+
+
+
---
+++ ServiceLayer/Services/FibonacciService.cs
@@ -0,0 +1,19 @@
+﻿using ServiceLayer.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class FibonacciService : IFibonacciService
+    {
+        public int Fibonacci()
+        {
+            return 0;
+        }
+
+    }
+    
+}
